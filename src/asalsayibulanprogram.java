5497ccbd90bd5cadcdc29069f429a8638efcf805
VMA9 <oooaliooo.mav@gmail.com>
2022-10-11 14:38:51 +0300

    first commit

--- src/asalsayibulanprogram.java
+++ src/asalsayibulanprogram.java
@@ -1,10 +1,10 @@
 public class asalsayibulanprogram {
     public static void main(String[] args) {
-        int result;
+
         for (int i = 1; i <= 100; i++) {
             int counter = 0;
             for (int x = 1; x <= i; x++) {
-                result = i % x;
+                int result = i % x;
                 if (result == 0) {
                     counter++;
                 }
